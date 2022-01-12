43107117174021d3c915a7315b7903bd7e879c48
“Mateus <MateusGcMiguel@outlook.com>
2022-01-12 11:52:14 -0300

    100% Requisitos obrigatorios

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const button = document.getElementById('buttonHeader');
 const email = document.getElementById('email');
 const senha = document.getElementById('senha');
+const checkBox = document.getElementById('agreement');
+const envio = document.getElementById('submit-btn')
 
 function headerForm() {
   const confEmail = email.value;
@@ -11,5 +13,11 @@ function headerForm() {
     alert('Email ou senha inválidos.');
   }
 }
+function btnCheck() {
+  envio.disabled = !envio.disabled;
+}
 
+envio.disabled = true;
+
+checkBox.addEventListener('click', btnCheck)
 button.addEventListener('click', headerForm);
