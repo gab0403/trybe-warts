ee24b8ea40f71be348a3cdf8f6134c1749f1ae3d
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-01-17 15:47:57 -0300

    organiza js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const button = document.getElementById('buttonHeader');
 const email = document.getElementById('email');
 const senha = document.getElementById('senha');
-const checkBox = document.getElementById('agreement');
+const checkbox = document.getElementById('agreement');
 const envio = document.getElementById('submit-btn');
 
 function headerForm() {
@@ -13,11 +13,19 @@ function headerForm() {
     alert('Email ou senha inválidos.');
   }
 }
+
 function btnCheck() {
   envio.disabled = !envio.disabled;
 }
 
 envio.disabled = true;
 
-checkBox.addEventListener('click', btnCheck);
+function cancelSubmt() {
+  envio.addEventListener('click', (e) => {
+    e.preventDefault();
+  });
+}
+cancelSubmt();
+
+checkbox.addEventListener('click', btnCheck);
 button.addEventListener('click', headerForm);
